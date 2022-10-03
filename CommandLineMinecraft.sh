6a93e68c2820b1eee419a580c20edc49d22662a3
DemoVPS69420 <111243610+DemoVPS69420@users.noreply.github.com>
2022-10-03 11:12:03 +0700

    Update CommandLineMinecraft.sh

--- CommandLineMinecraft.sh
+++ CommandLineMinecraft.sh
@@ -1,1 +1,1 @@
-clear && tailscale up && clear && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.30.04.zip && unzip bedrock-server-1.19.30.04.zip && rm bedrock-server-1.19.30.04.zip && ./bedrock_server
+clear && tailscale up && clear && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.30.04.zip && unzip bedrock-server-1.19.30.04.zip && rm bedrock-server-1.19.30.04.zip && clear && ./bedrock_server
